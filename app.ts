2d29724700e8bfdb074b32322b947553489620db
Andra <andradwikasa@gmail.com>
2023-08-04 21:57:41 +0700

    commit to deploy

--- app.ts
+++ app.ts
@@ -1,22 +1,104 @@
-let balance: number;
-
-function addBalance(value: number): number {
-    return balance + value;
-};
-
-function subtractBalance(value: number): number {
-    return balance - value;
-};
-
+//Import html element with DOM
+const typeSelect = document.getElementById("type") as HTMLSelectElement;
+const amountInput = document.getElementById("amount") as HTMLInputElement;
+const detailsInput = document.getElementById("details") as HTMLInputElement;
+const list = document.getElementById("transactionList") as HTMLUListElement;
 const addButton = document.getElementById("addButton") as HTMLButtonElement;
+const balanceDisplay: HTMLElement | null = document.getElementById("balance")!;
+const totalIncomeDisplay: HTMLElement | null =
+  document.getElementById("income")!;
+const totalExpensesDisplay: HTMLElement | null =
+  document.getElementById("expenses")!;
+interface transaction {
+  details: string;
+  amount: number;
+}
+
+interface account {
+  name: string;
+  balance: number;
+  totalIncome: number;
+  totalExpenses: number;
+}
+
+let accountProfile: account = {
+  name: "",
+  balance: 0,
+  totalIncome: 0,
+  totalExpenses: 0,
+};
+
+balanceDisplay.textContent = `$${accountProfile.balance.toFixed(2)}`;
+totalIncomeDisplay.textContent = `$${accountProfile.totalIncome.toFixed(2)}`;
+totalExpensesDisplay.textContent = `$${accountProfile.totalExpenses.toFixed(
+  2
+)}`;
+
+function addBalance(amount: number): number {
+  accountProfile.balance += amount;
+  return accountProfile.balance;
+}
+
+function subtractBalance(amount: number): number {
+  accountProfile.balance -= amount;
+  return accountProfile.balance;
+}
+
+function addTotalIncome(amount: number): number {
+  accountProfile.totalIncome += amount;
+  return accountProfile.totalIncome;
+}
+
+function addTotalExpenses(amount: number): number {
+  accountProfile.totalExpenses += amount;
+  return accountProfile.totalExpenses;
+}
+
+const transactionList: transaction[] = [];
+
+function inputTransaction() {
+  const details = detailsInput.value;
+  const amount = parseFloat(amountInput.value);
+
+  const newTransactionList: transaction = {
+    details: details,
+    amount: amount,
+  };
+
+  transactionList.push(newTransactionList);
+}
+
+function displayTransaction() {
+  list.innerHTML = "";
+  transactionList.forEach((transaction, index) => {
+    const li = document.createElement("li");
+    li.textContent = `${transaction.details} - Amount: ${transaction.amount}`;
+    list.appendChild(li);
+  });
+}
 
 addButton?.addEventListener("click", (event) => {
-    event.preventDefault();
-
-    const amountInput = document.getElementById("amount") as HTMLInputElement;
-    const amountValue = amountInput.value;
-    const list = document.getElementById("transactionList") as HTMLUListElement;
-    const listItem = document.createElement("li");
-    listItem.textContent = amountValue;
-    list?.appendChild(listItem);
+  event.preventDefault();
+  if (amountInput.value != "" && detailsInput.value != "") {
+    inputTransaction();
+    displayTransaction();
+    if (typeSelect.value == "cash in") {
+      addTotalIncome(parseFloat(amountInput.value));
+      addBalance(parseFloat(amountInput.value));
+    } else {
+      addTotalExpenses(parseFloat(amountInput.value));
+      subtractBalance(parseFloat(amountInput.value));
+    }
+    balanceDisplay.textContent = `$${accountProfile.balance.toFixed(2)}`;
+    totalIncomeDisplay.textContent = `$${accountProfile.totalIncome.toFixed(
+      2
+    )}`;
+    totalExpensesDisplay.textContent = `$${accountProfile.totalExpenses.toFixed(
+      2
+    )}`;
+    amountInput.value = "";
+    detailsInput.value = "";
+  } else {
+    alert("isi woi");
+  }
 });
--- script/app.js
+++ script/app.js
@@ -1,20 +1,76 @@
 "use strict";
-let balance;
-function addBalance(value) {
-    return balance + value;
-}
-;
-function subtractBalance(value) {
-    return balance - value;
-}
-;
+//Import html element with DOM
+const typeSelect = document.getElementById("type");
+const amountInput = document.getElementById("amount");
+const detailsInput = document.getElementById("details");
+const list = document.getElementById("transactionList");
 const addButton = document.getElementById("addButton");
+const balanceDisplay = document.getElementById("balance");
+const totalIncomeDisplay = document.getElementById("income");
+const totalExpensesDisplay = document.getElementById("expenses");
+let accountProfile = {
+    name: "",
+    balance: 0,
+    totalIncome: 0,
+    totalExpenses: 0,
+};
+balanceDisplay.textContent = `$${accountProfile.balance.toFixed(2)}`;
+totalIncomeDisplay.textContent = `$${accountProfile.totalIncome.toFixed(2)}`;
+totalExpensesDisplay.textContent = `$${accountProfile.totalExpenses.toFixed(2)}`;
+function addBalance(amount) {
+    accountProfile.balance += amount;
+    return accountProfile.balance;
+}
+function subtractBalance(amount) {
+    accountProfile.balance -= amount;
+    return accountProfile.balance;
+}
+function addTotalIncome(amount) {
+    accountProfile.totalIncome += amount;
+    return accountProfile.totalIncome;
+}
+function addTotalExpenses(amount) {
+    accountProfile.totalExpenses += amount;
+    return accountProfile.totalExpenses;
+}
+const transactionList = [];
+function inputTransaction() {
+    const details = detailsInput.value;
+    const amount = parseFloat(amountInput.value);
+    const newTransactionList = {
+        details: details,
+        amount: amount,
+    };
+    transactionList.push(newTransactionList);
+}
+function displayTransaction() {
+    list.innerHTML = "";
+    transactionList.forEach((transaction, index) => {
+        const li = document.createElement("li");
+        li.textContent = `${transaction.details} - Amount: ${transaction.amount}`;
+        list.appendChild(li);
+    });
+}
 addButton?.addEventListener("click", (event) => {
     event.preventDefault();
-    const amountInput = document.getElementById("amount");
-    const amountValue = amountInput.value;
-    const list = document.getElementById("transactionList");
-    const listItem = document.createElement("li");
-    listItem.textContent = amountValue;
-    list?.appendChild(listItem);
+    if (amountInput.value != "" && detailsInput.value != "") {
+        inputTransaction();
+        displayTransaction();
+        if (typeSelect.value == "cash in") {
+            addTotalIncome(parseFloat(amountInput.value));
+            addBalance(parseFloat(amountInput.value));
+        }
+        else {
+            addTotalExpenses(parseFloat(amountInput.value));
+            subtractBalance(parseFloat(amountInput.value));
+        }
+        balanceDisplay.textContent = `$${accountProfile.balance.toFixed(2)}`;
+        totalIncomeDisplay.textContent = `$${accountProfile.totalIncome.toFixed(2)}`;
+        totalExpensesDisplay.textContent = `$${accountProfile.totalExpenses.toFixed(2)}`;
+        amountInput.value = "";
+        detailsInput.value = "";
+    }
+    else {
+        alert("isi woi");
+    }
 });
